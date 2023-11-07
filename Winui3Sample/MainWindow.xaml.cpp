26b31f62f57caab9f8cb7b926b7c7f1c99ca3707
tetrisplusplus <shinji.ipad123@icloud.com>
2023-11-07 22:02:57 +0900

    add swapchainpanel

--- Winui3Sample/MainWindow.xaml.cpp
+++ Winui3Sample/MainWindow.xaml.cpp
@@ -7,6 +7,8 @@
 #include "MainWindow.g.cpp"
 #endif
 
+#include <winrt/Windows.System.Threading.h>
+
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
@@ -30,8 +32,51 @@ namespace winrt::Winui3Sample::implementation
         throw hresult_not_implemented();
     }
 
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    void MainWindow::SwapChainPanel_Loading(winrt::Microsoft::UI::Xaml::FrameworkElement const& sender, winrt::Windows::Foundation::IInspectable const& args)
     {
-        myButton().Content(box_value(L"Clicked"));
+        Diligent::EngineD3D12CreateInfo createInfo;
+        auto* factory = Diligent::GetEngineFactoryD3D12();
+        factory->CreateDeviceAndContextsD3D12(createInfo, &device, &context);
+
+        Diligent::SwapChainDesc swapChainDesc{};
+        swapChainDesc.Width = 1;
+        swapChainDesc.Height = 1;
+
+        Diligent::Win32NativeWindow window{ winrt::get_abi(sender) };
+        factory->CreateSwapChainD3D12(device, context, swapChainDesc, Diligent::FullScreenModeDesc{}, window, &swapchain);
+    }
+
+
+    void MainWindow::SwapChainPanel_SizeChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::SizeChangedEventArgs const& e)
+    {
+        auto size = e.NewSize();
+        const UINT width = UINT(size.Width);
+        const UINT height = UINT(size.Height);
+
+        this->swapchain->Resize(width, height);
+    }
+
+    void MainWindow::SwapChainPanel_Loaded(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+    {
+        this->StartRenderLoop();
+    }
+
+    void MainWindow::StartRenderLoop()
+    {
+        auto workItemHandler = winrt::Windows::System::Threading::WorkItemHandler([this](winrt::Windows::Foundation::IAsyncAction const& action)
+            {
+                while (action.Status() == Windows::Foundation::AsyncStatus::Started)
+                {
+                    auto* pRtv = this->swapchain->GetCurrentBackBufferRTV();
+
+                    const float ClearColor[]{ 0.2f, 0.9f, 0.2f, 1.0f };
+                    this->context->ClearRenderTarget(pRtv, ClearColor, Diligent::RESOURCE_STATE_TRANSITION_MODE_TRANSITION);
+
+                    this->swapchain->Present();
+                }
+            });
+
+        using namespace Windows::System::Threading;
+        this->renderLoopWorker = Windows::System::Threading::ThreadPool::RunAsync(workItemHandler, WorkItemPriority::High, WorkItemOptions::TimeSliced);
     }
 }
--- Winui3Sample/MainWindow.xaml.h
+++ Winui3Sample/MainWindow.xaml.h
@@ -5,6 +5,10 @@
 
 #include "MainWindow.g.h"
 
+#include <Common/interface/RefCntAutoPtr.hpp>
+#include <Graphics/GraphicsEngineD3D12/interface/EngineFactoryD3D12.h>
+#include <Graphics/GraphicsEngine/interface/SwapChain.h>
+
 namespace winrt::Winui3Sample::implementation
 {
     struct MainWindow : MainWindowT<MainWindow>
@@ -14,7 +18,21 @@ namespace winrt::Winui3Sample::implementation
         int32_t MyProperty();
         void MyProperty(int32_t value);
 
-        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void SwapChainPanel_Loading(winrt::Microsoft::UI::Xaml::FrameworkElement const& sender, winrt::Windows::Foundation::IInspectable const& args);
+
+        void SwapChainPanel_SizeChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::SizeChangedEventArgs const& e);
+
+        void SwapChainPanel_Loaded(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+
+        void StartRenderLoop();
+
+    private:
+        Diligent::RefCntAutoPtr<Diligent::IRenderDevice>  device;
+        Diligent::RefCntAutoPtr<Diligent::IDeviceContext> context;
+        Diligent::RefCntAutoPtr<Diligent::ISwapChain>     swapchain;
+        Diligent::RefCntAutoPtr<Diligent::IPipelineState> pso;
+
+        winrt::Windows::Foundation::IAsyncAction renderLoopWorker;
     };
 }
 
